6275582012196085849412d7a4d27589d58a1418
LavanyaLahari811 <lavanyalaharik2003@gmail.com>
2024-01-30 18:11:16 +0530

    added sound effects

--- index.js
+++ index.js
@@ -35,8 +35,19 @@ class Raven {
     this.maxFrame = 4;
     this.flapTime = 0;
     this.flapInterval = Math.random() * 50 + 50;
-    this.randomColors=[Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255)];
-    this.color="rgb("+this.randomColors[0]+","+this.randomColors[1]+","+this.randomColors[2]+")"
+    this.randomColors = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+    ];
+    this.color =
+      "rgb(" +
+      this.randomColors[0] +
+      "," +
+      this.randomColors[1] +
+      "," +
+      this.randomColors[2] +
+      ")";
   }
   update(deltaTime) {
     if (this.y < 0 || this.y > canvas.height - this.height) {
@@ -58,8 +69,8 @@ class Raven {
     }
   }
   draw() {
-    collisionCtx.fillStyle=this.color;
-    collisionCtx.fillRect(this.x,this.y,this.width,this.height);
+    collisionCtx.fillStyle = this.color;
+    collisionCtx.fillRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       this.image,
       this.frame * this.spriteWidth,
@@ -74,6 +85,41 @@ class Raven {
   }
 }
 
+let explosions = [];
+class Explosion {
+  constructor(x, y, size) {
+    this.image = new Image();
+    this.image.src = "./boom.png";
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.size=size;
+    this.x = x;
+    this.y = y;
+    this.frame = 0;
+    this.sound = new Audio();
+    this.sound.src = "./boom.wav";
+    this.timeSinceLastFrame = 0;
+    this.frameInterval = 100;
+    this.markedForDelection=false;
+  }
+  update(deltaTime) {
+    this.timeSinceLastFrame += deltaTime;
+    if (this.frame == 0) {
+      this.sound.play();
+    }
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++;
+      if(this.frame>5){
+        this.markedForDelection=true;
+      }
+      this.timeSinceLastFrame=0;
+    }
+  }
+  draw(){
+    ctx.drawImage(this.image,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,this.x,this.y-this.size*0.25,this.size,this.size);
+  }
+}
+
 const drawScore = () => {
   ctx.fillStyle = "black";
   ctx.fillText("Score:" + score, 50, 75);
@@ -81,16 +127,22 @@ const drawScore = () => {
   ctx.fillText("Score:" + score, 55, 80);
 };
 
-window.addEventListener("click",(event)=>{
-   const detectPixelColor=collisionCtx.getImageData(event.x,event.y,1,1);
-   const clr=detectPixelColor.data;
-   ravens.forEach(obj=>{
-    if(obj.randomColors[0]==clr[0] && obj.randomColors[1]==clr[1] && obj.randomColors[2]==clr[2]){
-        obj.markedForDelection=true;
-        score++;
+window.addEventListener("click", (event) => {
+  const detectPixelColor = collisionCtx.getImageData(event.x, event.y, 1, 1);
+  const clr = detectPixelColor.data;
+  ravens.forEach((obj) => {
+    if (
+      obj.randomColors[0] == clr[0] &&
+      obj.randomColors[1] == clr[1] &&
+      obj.randomColors[2] == clr[2]
+    ) {
+      obj.markedForDelection = true;
+      score++;
+      explosions.push(new Explosion(obj.x,obj.y,obj.width));
+      console.log(explosions);
     }
-   })
-})
+  });
+});
 
 const animate = (timeStamp) => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -101,18 +153,19 @@ const animate = (timeStamp) => {
   if (timeToNextRaven > ravenInterval) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
-    ravens.sort((a,b)=>{
-        return a.width-b.width;
-    })
+    ravens.sort((a, b) => {
+      return a.width - b.width;
+    });
   }
   drawScore();
-  [...ravens].forEach((obj) => {
+  [...ravens,...explosions].forEach((obj) => {
     obj.draw();
   });
-  [...ravens].forEach((obj) => {
+  [...ravens,...explosions].forEach((obj) => {
     obj.update(deltaTime);
   });
   ravens = ravens.filter((obj) => !obj.markedForDelection);
+  explosions=explosions.filter((obj)=>!obj.markedForDelection);
   requestAnimationFrame(animate);
 };
 animate(0);
